c30c01c3209d56889151c33743b881f481f881d3
Jinhyuk <kobin1970@gmail.com>
2024-03-14 15:08:56 +0100

    remove tanstack

--- components/layouts/QueryClientProvider.tsx
+++ components/layouts/QueryClientProvider.tsx
@@ -1,13 +1,13 @@
-"use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// "use client";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
-export default function QueryClientProviderComponent({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
-}
+// const queryClient = new QueryClient();
+// export default function QueryClientProviderComponent({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+//   );
+// }
